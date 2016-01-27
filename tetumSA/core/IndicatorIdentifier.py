7646b22057a0dab1ad309aabba8dc636bd7f8f99
nhthien <nhthien@jaist.ac.jp>
2016-01-27 19:06:56 +0700

    Add indicator identifier.

--- tetumSA/core/IndicatorIdentifier.py
+++ tetumSA/core/IndicatorIdentifier.py
@@ -13,34 +13,49 @@ from sklearn.semi_supervised import LabelPropagation
 from sklearn.semi_supervised import LabelSpreading
 import random
 import TetumPreprocessing as tp
-class SentimentAnalysis(object):
+import pandas as pd
+from pprint import pprint
+from nltk import FreqDist
+
+class IndicatorIdentifier(object):
     '''
     classdocs
     '''
 
 
     def __init__(self):
-        self.model = LabelPropagation()#(kernel='knn', alpha=1.0)
+        self.model = LabelPropagation() #(kernel='knn', alpha=1.0)
         #self.model = LabelSpreading()
     
     def readingDatabase(self):
         da = DocumentsAccess()
         
-        filePos = "Database/Sentiment/Sentiment/PoliceRelations/positive.xlsx"
+        labeledFile = "Database/Indicator/Indicators.xlsx"
         sheet = "Sheet1"
-        posData = da.readingDatabaseTetum(filePos, sheet)
-        posData = posData[0].tolist()
-        print len(posData)
-        print posData[0]
+        df = da.readingDatabaseTetum(labeledFile, sheet, head= 0)
 
-        
- 
-        fileNeg = "Database/Sentiment/Sentiment/PoliceRelations/negative.xlsx"
-        sheet = "Sheet1"
-        negData = da.readingDatabaseTetum(fileNeg, sheet)  
-        negData = negData[0].tolist() 
-        print len(negData)
-        print negData[0]
+
+        cut = int(0.8*df.shape[0])    
+        # re-duplicate the data => Result: one document has one label only
+        columns = df.columns.tolist()
+        columns.remove("Content")
+        print columns
+        X_train = []
+        Y_train = []
+        X_test = []
+        Y_test = []
+        for index, row in df.iterrows():
+            labels = list(set([row[col] for col in columns if not pd.isnull(row[col])]))
+            content = row["Content"]
+            if index < cut: # training part
+                for label in labels:
+                    X_train.append(content)
+                    Y_train.append(label)
+            else:
+                X_test.append(content)
+                Y_test.append(labels)
+                
+           
        
         fileUnlabeled = "Database/Clean Master Cleaner 2222.xlsx"
         sheet = "Sheet1"
@@ -67,10 +82,9 @@ class SentimentAnalysis(object):
         unlabeledData = unlabeledData + unlabeledData2 + unlabeledData3
         
         print len(unlabeledData)
-        print unlabeledData[0]
-
-       
-        return (posData, negData, unlabeledData) 
+        #print unlabeledData[0]
+        
+        return (X_train, Y_train, X_test, Y_test, unlabeledData) 
     
     def preprocessData(self, X):   
         return tp.preprocess_dataset(X, fold=True, specials=False, min_size=2)       
@@ -123,36 +137,63 @@ class SentimentAnalysis(object):
        
     def run(self):
         # Reading data
-        (posData, negData, unlabeledData)  = self.readingDatabase()
+        (X_train, Y_train, X_test, Y_test, unlabeledData)   = self.readingDatabase()
+        print "Training size: " + str(len(X_train))
+        print "Test size: " + str(len(X_test))
+        '''
+        X_train = X_train[:100]
+        Y_train = Y_train[:100]
+        X_test = X_test[:100]
+        Y_test = Y_test[:100]
+        '''
         print "Finish reading database."
-           
-        # Divide training and test data
-        cut = 10
-        posDataTrain = posData[:cut]
-        negDataTrain = negData[:cut]
-        posDataTest = posData[cut:]
-        negDataTest = negData[cut:]        
+        #print FreqDist(indicators).most_common()
+        k = 0
+        dictLabel = FreqDist(Y_train)
+        for key in dictLabel:
+            dictLabel[key] = k
+            k+=1
+        Y_train = [dictLabel[ind] for ind in Y_train]
+        Y_test = [[dictLabel[ind] for ind in labels] for labels in Y_test]
         
+        '''       
         random.seed(123456)
         # Training
-        X_train = posDataTrain + negDataTrain + unlabeledData
-        Y_train = np.ones((len(posDataTrain)), dtype = int).tolist() + np.zeros((len(negDataTrain)), dtype = int).tolist() + (-1*np.ones((len(unlabeledData)), dtype = int)).tolist()
-        z = zip(X_train, Y_train)
+        z = zip(labeledData, indicators)
         random.shuffle(z)
-        X_train, Y_train = zip(*z)
+        labeledData, indicators = zip(*z)
+        
+        X_train = list(labeledData[:cut])
+        Y_train = list(indicators[:cut])
+        X_test = list(labeledData[cut:])
+        Y_test = list(indicators[cut:])
+        '''
+        X_train += unlabeledData
+        Y_train += (-1*np.ones((len(unlabeledData)), dtype = int)).tolist()
+
+        #pprint(X_train)
+        #print Y_train
+        
+        #print X_train[cut-2:cut+2]
+        #print Y_train[cut-2:cut+2]
+        print "Training..."
         self.train(X_train, Y_train)
         
         # Testing
-        X_test = posDataTest + negDataTest
-        Y_test = np.ones((len(posDataTest)), dtype = int).tolist() + np.zeros((len(negDataTest)), dtype = int).tolist()
-        z = zip(X_test, Y_test)
-        random.shuffle(z)
-        X_test, Y_test = zip(*z)
+        print "Testing..."
         Y_predicted = self.test(X_test)
+
         print Y_predicted
 
-        (accuracy,precision,recall,f1) = self.evaluation(Y_test,Y_predicted)
-        print (accuracy,precision,recall,f1)  
+        # The Y_predicted only need to be one of the true labels in order to be calculated as correctness
+        for i in range(len(Y_predicted)):
+            lab = Y_predicted[i]
+            if lab in Y_test[i]:
+                Y_test[i] = lab
+            else:
+                Y_test[i] = -1
+        (accuracy,_, _, _) = self.evaluation(Y_test,Y_predicted)
+        print accuracy  
 
     
 
@@ -164,8 +205,8 @@ def main():
     startTime = datetime.now()
     
         
-    sentimentAnalysis = SentimentAnalysis()
-    sentimentAnalysis.run()
+    ii = IndicatorIdentifier()
+    ii.run()
  
     elapsedTime = datetime.now() - startTime 
     totSecs = elapsedTime.total_seconds()
--- tetumSA/databaseAccess/DocumentsAccess.py
+++ tetumSA/databaseAccess/DocumentsAccess.py
@@ -10,37 +10,14 @@ import random
 import pandas as pd
 from pprint import pprint
 #----------------------------------------------------------------------
-class DocumentsAccess():   
-    # Return array of sentences in the directory
-    def parseFromDirectory(self, directoryPath):
-        arrDocuments = []
-        for name in os.listdir(directoryPath):
-            if os.path.isfile(os.path.join(directoryPath, name)):
-                with open(os.path.join(directoryPath, name)) as myfile:
-                    document = myfile.read()
-                    arrDocuments.append(document)
-        return arrDocuments     
-    
-    def readingDatabaseEn(self):
-        posDocs = DocumentsAccess.parseFromDirectory('Database/txt_sentoken/pos/')
-        negDocs = DocumentsAccess.parseFromDirectory('Database/txt_sentoken/neg/')
-        posLabels = np.ones(len(posDocs)).tolist()
-        negLabels = (-1 * np.ones(len(negDocs))).tolist()
-        allDocs = posDocs + negDocs
-        allLabels = posLabels + negLabels
-        
-        z = zip(allDocs, allLabels)
-        random.seed(12345)
-        random.shuffle(z)
-        allDocs, allLabels = zip(*z)
-        return allDocs, allLabels    
-       
-    def readingDatabaseTetum(self, filePath, sheet):
+class DocumentsAccess():          
+    def readingDatabaseTetum(self, filePath, sheet, head = None):
         xl = pd.ExcelFile(filePath)
-        df = xl.parse(sheet, header=None)
+        df = xl.parse(sheet, header=head)
         df.head()
         return df
-              
+    
+       
 #----------------------------------------------------------------------
 if __name__ == "__main__":
 
